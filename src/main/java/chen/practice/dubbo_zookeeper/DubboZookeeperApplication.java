ea789dfabf6735d5c28accfea427c2d08eb09caf
Robert Chen <734702243@qq.com>
2021-05-13 15:24:56 +0800

    first commit

---
+++ src/main/java/chen/practice/dubbo_zookeeper/DubboZookeeperApplication.java
@@ -0,0 +1,13 @@
+package chen.practice.dubbo_zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboZookeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chen/practice/dubbo_zookeeper/DubboZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package chen.practice.dubbo_zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
